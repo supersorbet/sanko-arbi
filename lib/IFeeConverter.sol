41be7ed3abeaf54f0558ea1307d181dfe883603c
supersorbet <artificialperiod@gmail.com>
2024-02-10 02:04:53 -0500

    first commit

---
+++ lib/IFeeConverter.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: UNLICENSE
+pragma solidity ^0.8.21;
+
+import {IERC20} from "./forge-std/src/interfaces/IERC20.sol";
+import {Ownable} from "solady/src/auth/Ownable.sol";
+import {ICamelotRouter} from "./ICamelotRouter.sol";
+
+interface IFeeConverter {
+    function setFeeToken(address _feeToken) external;
+    function setRouter(address _router) external;
+    function setUsdc(address _usdc) external;
+
+    // Withdraw collected oracle fees to a recipient
+    function withdrawFees() external;
+    function convertFees() external payable returns (bool);
+}
